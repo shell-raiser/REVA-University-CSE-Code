a4ff8d4647323374598ceb05259257dfbeabc3a2
Shell Raiser <ksshailesh05@gmail.com>
2023-06-07 11:30:34 +0530

    fix oddPosSort

--- placementTraining/7.6/sortingOddPos.java
+++ placementTraining/7.6/sortingOddPos.java
@@ -1,7 +1,7 @@
 public class sortingOddPos {
     public static void main(String[] args) {
         int arr[] = { 12, 13, 1, 2, 11, 4 };
-        for (int i = 0; i < 6; i+=2) {
+        for (int i = 1; i < 6; i+=2) {
             for (int j = i + 2; j < 6; j+=2) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[j];
