13d81d3012fb302bcd202ff5626dfd9deb2e7df7
Shell Raiser <ksshailesh05@gmail.com>
2023-06-07 09:21:51 +0530

    go till n/2 only

--- placementTraining/7.6/reversePrime.java
+++ placementTraining/7.6/reversePrime.java
@@ -32,7 +32,7 @@ public class reversePrime {
     }
 
     public static boolean isPrime(int n) {
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n/2; i++) {
             if (n % i == 0) {
                 return false;
             }
