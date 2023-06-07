1e4ef9bfb94ed290e6adfc0c4eb877696c2bb603
Shell Raiser <ksshailesh05@gmail.com>
2023-06-07 09:07:21 +0530

    primePalindrome program

---
+++ placementTraining/7.6/reversePrime.java
@@ -0,0 +1,42 @@
+// find if given number is prime, if yes check if reverse of the number is also prime
+
+import java.util.Scanner;
+
+public class reversePrime {
+    public static void main(String[] args) {
+        // int n = 1399;
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        if (isPrime(n)) {
+            int rev = reverse(n);
+            System.out.println(rev);
+            if (isPrime(rev)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        } else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
+
+    public static int reverse(int n) {
+        int rev = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        return rev;
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
