4b75f704d92b577820bee8d4fa7c0c2cf5a46413
Shell Raiser <ksshailesh05@gmail.com>
2023-06-08 08:51:03 +0530

    using array sum method

--- placementTraining/8.6/missingElement.java
+++ placementTraining/8.6/missingElement.java
@@ -1,11 +1,24 @@
 public class missingElement {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 4, 5, 6 };
+        int sum1 = 0;
+        int sum2 = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] + 1 != arr[i + 1]) {
-                System.out.println((arr[i] + 1) + " Missing");
-                break;
-            }
+            // if (arr[i] + 1 != arr[i + 1]) {
+            // System.out.println((arr[i] + 1) + " Missing");
+            // break;
+            // }
+            sum1 += arr[i];
+        }
+        for (int i = 1; i <= arr[arr.length - 1]; i++) {
+            sum2 += i;
+        }
+
+        int missing = sum2 - sum1;
+        if (missing != 0) {
+            System.out.println(missing + " missing");
+        } else {
+            System.out.println("No Missing");
         }
     }
 }
