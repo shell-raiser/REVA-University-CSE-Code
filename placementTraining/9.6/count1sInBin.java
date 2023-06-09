01aa7d06e1fa99e89128dda36f13d0e8ccd5b6c1
Shell Raiser <ksshailesh05@gmail.com>
2023-06-09 11:18:43 +0530

    optimize

--- placementTraining/9.6/count1sInBin.java
+++ placementTraining/9.6/count1sInBin.java
@@ -2,8 +2,8 @@ public class count1sInBin {
     public static void main(String[] args) {
         String n = Integer.toBinaryString(25);
         int count = 0;
-        for (int i = 0; i < n.length(); i++) {
-            if (n.charAt(i) == '1') {
+        for (char i : n.toCharArray()) {
+            if (i == '1') {
                 count++;
             }
         }
